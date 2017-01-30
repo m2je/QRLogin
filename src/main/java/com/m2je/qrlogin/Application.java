5fe7844c4f3ee27b6fecd4f5a746d63552605d8a
mehdi <mehdis@hyperoffice.com>
2017-01-29 21:29:25 -0500

    fixing compiliation error

--- src/main/java/com/m2je/qrlogin/Application.java
+++ src/main/java/com/m2je/qrlogin/Application.java
@@ -1,11 +1,8 @@
 package com.m2je.qrlogin;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-import com.m2je.qrlogin.server.ServerService;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
